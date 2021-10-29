a57aa97fc1fbb3d6a1ff426addd30cdee1e4975b
AnthonyCampana <acampana18@gmail.com>
2021-10-29 06:52:45 -0400

    This is a test commit

--- recipeComments/models.py
+++ recipeComments/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 #class Users(models.Model):
-#    userName = models.CharField()
+#    firstName = models.CharField()
+#    lastName = models.CharField()
 #    password = models.CharField()
 #    email = models.CharField() 
 #    profilePhoto
