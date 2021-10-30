b102876c3af7164c7ee896db236bb4937c47f20e
PineappleBingo <brownkpt60@gmail.com>
2021-10-30 16:48:45 -0400

    index about template setup

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='site-home'),
-    path('about/', views.about, name='site-about'),
+    path('', views.home, name='main-home'),
+    path('about/', views.about, name='main-about'),
 ]
