8a108e34d644ee10e2b77439dd2fd34266fdfd5f
PineappleBingo <brownkpt60@gmail.com>
2021-10-30 18:41:58 -0400

    updates

--- LeftoverIngredients/urls.py
+++ LeftoverIngredients/urls.py
@@ -20,10 +20,10 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('register/', user_views.register, name='register'),
-    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('main.urls')),
     path('', include('recipe.urls')),
 ]
