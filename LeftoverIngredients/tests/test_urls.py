2f410e0004bdceb8181ca4f2e432b95c64e1dc9c
Nezar Vidad <nezarv2k@gmail.com>
2021-11-04 17:19:02 -0400

    added tests for main view urls (home, about, recipe, search)

--- LeftoverIngredients/tests/test_urls.py
+++ LeftoverIngredients/tests/test_urls.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from users.views import register, profile
+from django.contrib.auth.views import LoginView, LogoutView
+from main.views import home, about, recipe, search
 
 class TestUrls(SimpleTestCase):
 
@@ -13,11 +15,28 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, profile)
 
     def test_login_url_resolves(self):
-        assert 1 == 1
-        # url = reverse('register')
-        # self.assertEquals(resolve(url).func, login)
+        url = reverse('login')
+        self.assertEquals(resolve(url).func.view_class, LoginView)
 
     def test_logout_url_resolves(self):
-        assert 1 == 1
-        # url = reverse('register')
-        # self.assertEquals(resolve(url).func, logout)
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.view_class, LogoutView)
+
+    def test_home_url_resolves(self):
+        url = reverse('main-home')
+        self.assertEquals(resolve(url).func, home)
+
+        
+    def test_about_url_resolves(self):
+        url = reverse('main-about')
+        self.assertEquals(resolve(url).func, about)
+
+        
+    def test_recipe_url_resolves(self):
+        url = reverse('main-recipe')
+        self.assertEquals(resolve(url).func, recipe)
+
+        
+    def test_search_url_resolves(self):
+        url = reverse('main-search')
+        self.assertEquals(resolve(url).func, search)
