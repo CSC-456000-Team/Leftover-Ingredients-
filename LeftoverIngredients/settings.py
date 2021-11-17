00b86c14c6723bf8217c8757cff7d304d34348c9
Nezar Vidad <nezarv2k@gmail.com>
2021-11-17 14:50:22 -0500

    modified settings

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -118,11 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    "/main/static",
-]
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 
