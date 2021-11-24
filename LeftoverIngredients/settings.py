6e1b114226fbeceea274b191e2fb176744645488
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-24 18:07:25 -0500

    Reset password feature frontend/backend structure

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -150,3 +150,12 @@ django_heroku.settings(locals())
 SESSION_COOKIE_AGE = (
     60 * 60 * 24 * 30 * 12
 )  # 12 Months (Months are 30days so 360 days in total)
+
+
+# Reset Password
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
