9b605191726ce47e06fefb71c18db7b554c53957
David B <Davbala@gmail.com>
2021-11-24 19:36:24 -0500

    Added Email Environment Variables
    Added email environment variables

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -36,8 +36,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tttesttting6@gmail.com'
-EMAIL_HOST_PASSWORD = 'testing123!'
+EMAIL_HOST_USER = env['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
 
 
 # Application definition
