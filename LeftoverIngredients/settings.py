11edfcc40d83e2cf1d4c77cc2e02ffab82654587
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 13:22:44 -0500

    Setting Static URL for Heroku

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_ = "/static/"
+STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
