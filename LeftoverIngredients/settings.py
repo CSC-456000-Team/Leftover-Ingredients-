b9d668d9e81b767918e89ef0ee6ad9176e1cd3ee
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 02:36:52 -0500

    Update AWS Settings

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -137,9 +137,17 @@ LOGIN_URL = "login"
 # AWS
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAE_BUCKET_NAME = os.environ.get("AWS_STORAE_BUCKET_NAME")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
+AWS_S3_REGION_NAME = "us-east-2"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Testing
+# print(os.environ.get('AWS_ACCESS_KEY_ID'))
+# print(os.environ.get('AWS_SECRET_ACCESS_KEY'))
+# print(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
