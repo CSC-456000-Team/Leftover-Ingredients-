1d117dd6410d776ae2428e7b54a811d7085fa2d7
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 01:47:23 -0500

    Connect AWS S3

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,14 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "main-home"
 
 LOGIN_URL = "login"
+
+
+# AWS
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAE_BUCKET_NAME = os.environ.get("AWS_STORAE_BUCKET_NAME")
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
