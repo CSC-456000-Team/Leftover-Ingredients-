e74fca267c7ecd976a8cc9f4b421883a4e416968
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 23:33:47 -0500

    removing staticfiles_dir

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
