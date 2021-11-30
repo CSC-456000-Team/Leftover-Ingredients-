c2fbff89f932ff343dbade4e5ea6faac5f91894d
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 23:48:01 -0500

    updates static path

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/main/static/"
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
