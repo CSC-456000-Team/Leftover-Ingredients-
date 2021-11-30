eec8fe0e56e364d3f6fe944f8e2ca532316bc846
Jinho(David) Seo <brownkpt60@gmail.com>
2021-11-29 23:29:51 -0500

    Updates settings for static files

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
