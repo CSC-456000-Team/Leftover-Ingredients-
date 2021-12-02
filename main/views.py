cc1f055db67deff08e843f87dc4948ab05bbd66a
Nezar Vidad <nezarv2k@gmail.com>
2021-12-02 17:00:44 -0500

    basic recipe search implemented

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .connect_api import get_recipes
 
 # from django.http import HttpResponse
 
@@ -9,3 +10,14 @@ def home(request):
 
 def about(request):
     return render(request, "main/about.html", {"title": "About"})
+
+
+def search(request):
+    if request.method == "POST":
+        searched = request.POST["searched"]
+        recipes = get_recipes(searched)
+        return render(
+            request, "main/search.html", {"searched": searched, "recipes": recipes}
+        )
+    else:
+        return render(request, "main/search.html", {})
