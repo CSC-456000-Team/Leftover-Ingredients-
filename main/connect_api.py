9441b301f6db5b9bc0f84505d65818ff3da1641f
Nezar Vidad <nezarv2k@gmail.com>
2021-12-02 16:11:17 -0500

    defined functions that get recipes based on search query

--- main/connect_api.py
+++ main/connect_api.py
@@ -1,23 +1,21 @@
-from django.conf import settings
-from django.shortcuts import redirect
-from urllib.parse import urlencode
 import requests
+import json
 
 
 API_KEY = "9f97e9f457aa4379ba2cb4c32072aec4"
 
 # Gets the id's of recipes containing the searched ingredients, ingredients must be a comma-separated list
 def get_recipes_ids(ingredients):
-    search_url = f"https://api.spoonacular.com/recipes/findByIngredients?{ingredients}&number=5&apiKey={API_KEY}"
-
-    recipe_ids = []
+    search_url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&number=3&apiKey={API_KEY}"
 
     r = requests.get(search_url)
 
-    if r.status_code == 200:
-        r_dict = r.json()
-        for recipe in r_dict:
-            recipe_ids.append(recipe["id"])
+    results = json.loads(r.text)
+
+    recipe_ids = []
+
+    for recipe in results:
+        recipe_ids.append(recipe["id"])
 
     return recipe_ids
 
@@ -27,17 +25,19 @@ def get_recipes(recipe_ids):
     recipes = []
 
     for recipe_id in recipe_ids:
-        results_url = f"https://api.spoonacular.com/recipes/{recipe_id}/information&apiKey={API_KEY}"
+        info_url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}"
+        r = requests.get(info_url)
+        results = json.loads(r.text)
 
-        r = requests.get(results_url)
+        title = results["title"]
+        image = results["image"]
+        recipe_url = results["sourceUrl"]
 
-        if r.status_code == 200:
-            r_dict = r.json()
-            for recipe in r_dict:
-                name = recipe["title"]
-                image = recipe["image"]
-                source_url = recipe["sourceUrl"]
-                recipe_info = {"name": name, "image": image, "source_url": source_url}
-                recipes.append(recipe_info)
+        recipe = {"title": title, "image": image, "recipe_url": recipe_url}
+
+        recipes.append(recipe)
 
     return recipes
+
+
+print(get_recipes(get_recipes_ids("apple, banana")))
