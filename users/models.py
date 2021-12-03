8dfabe0d081e833e29eef1b6db6b43f494484380
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-02 17:46:10 -0800

    debug-login-register

--- users/models.py
+++ users/models.py
@@ -12,12 +12,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # Below code conflict with AWS S3 buckect
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
