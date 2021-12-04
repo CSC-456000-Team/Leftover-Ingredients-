90ed1c009471c4d2a17e883c758cec95c078594b
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-04 10:42:02 -0500

    updates settings

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "e5e2eb0889b637b654ca2bc30f40fc5a79dbb4da26240648"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
