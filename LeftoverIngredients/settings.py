eca298431498a25bcd5d00ddcf226642eb12b0cc
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-05 10:21:24 -0500

    re-connect heroku db

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -115,8 +115,8 @@ DATABASES = {
     #     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES["default"].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 #     "default": {
 #                 "ENGINE": "django.db.backends.postgresql_psycopg2",
