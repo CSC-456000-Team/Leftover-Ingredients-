11666ef431c1b592364a06620cd66822f924ecb4
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-05 10:06:40 -0500

    heroku db connection test

--- LeftoverIngredients/settings.py
+++ LeftoverIngredients/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,16 +104,19 @@ WSGI_APPLICATION = "LeftoverIngredients.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "DEMO_TEST",
-        "USER": "postgres",
-        "PASSWORD": "0564",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    #     "default": {
+    #         "ENGINE": "django.db.backends.postgresql",
+    #         "NAME": "DEMO_TEST",
+    #         "USER": "postgres",
+    #         "PASSWORD": "0564",
+    #         "HOST": "localhost",
+    #         "PORT": "5432",
+    #     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 #     "default": {
 #                 "ENGINE": "django.db.backends.postgresql_psycopg2",
