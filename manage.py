78ad96dee41c43b251e72ebea15c4022a36d2dd1
Nezar Vidad <nezarv2k@gmail.com>
2021-12-08 18:22:52 -0500

    commented out loadenv calls

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    load_dotenv()
+    # load_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LeftoverIngredients.settings")
     try:
         from django.core.management import execute_from_command_line
